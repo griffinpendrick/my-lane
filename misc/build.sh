2ca7bedf4a37340120e10cc73765044ac4c84825
Griffin Pendrick <griffinpendrick77@gmail.com>
2025-10-05 12:47:30 -0400

    added mac build

---
+++ misc/build.sh
@@ -0,0 +1,16 @@
+set -e
+
+OPENCV_PATH="/opt/homebrew/opt/opencv"
+BIN_DIR="$(pwd)/bin"
+SRC_DIR="$(pwd)/src"
+
+mkdir -p "$BIN_DIR"
+pushd "$BIN_DIR" > /dev/null
+
+CompilerFlags="-O0 -g -Wall -Wextra -Wno-unused-variable -Wno-unused-parameter"
+IncludePath="-I${OPENCV_PATH}/include/opencv4"
+LinkerFlags="-L${OPENCV_PATH}/lib -lopencv_core -lopencv_imgproc -lopencv_highgui -lopencv_videoio"
+
+clang++ $CompilerFlags $IncludePath "$SRC_DIR/main.cpp" $LinkerFlags -o mylane
+
+popd > /dev/null
